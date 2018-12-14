5adfd8f497cba9049c664a8eb4b3c191c89dc355
s-cooks <s-cooks@49S50013289.it.bsd405.org>
2018-12-14 11:14:28 -0800

    finish

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class FracCalc {
@@ -23,16 +24,65 @@ public class FracCalc {
     public static String produceAnswer(String input)
     {
     	//If the input is not formatted as expected, return an error message
-    	if(!input.matches("^-?[0-9]+((_[0-9]+/[1-9][0-9]*)|(/[1-9][0-9]*))? [\\-\\/*+] -?[0-9]+((_[0-9]+/[1-9][0-9]*)|(/[1-9][0-9]*))?$")) return "error: invalid entry";
+    	if(!input.matches("^-?[0-9]+((_[0-9]+/[1-9][0-9]*)|(/[1-9][0-9]*))?( [\\-\\/*+] -?[0-9]+((_[0-9]+/[1-9][0-9]*)|(/[1-9][0-9]*))?)+$")) return "error: invalid entry";
     	
     	//Split the input into the first operand, the operator, and the second operand
-    	String[] pieces = input.split(" ");
+    	ArrayList<String> pieces = new ArrayList<String>();
+    	for(String item : input.split(" "))
+    	{
+    		pieces.add(item);
+    	}
     	
+    		/*
+    		 * I made the program to only do single operations, and I was too lazy to 
+    		 * rewrite the whole thing for multiple operations, so what I'm doing
+    		 * is creating an ArrayList with all the parts of the expression and just
+    		 * passing each individual operation to the performOperation method one by one
+    		 * and replacing the three components of that operation in the ArrayList with
+    		 * the result. I also use order of operations.
+    		 * 
+    		 * One of the FracCalc tests does not properly follow order of operations which is why my program fails it.
+    		 */
+		//do all the multiplication and division pieces
+    	for(int i=1; i<pieces.size();i+=2)
+    	{
+    		if(pieces.get(i).matches("[\\/*]"))
+    		{
+    			//Replace the three parts of the operation with the result
+    			pieces.set(i-1,performOperation(pieces.get(i-1),pieces.get(i),pieces.get(i+1)));
+    			pieces.remove(i);
+    			pieces.remove(i);
+    			pieces.trimToSize();
+    			//If the result was a divide by 0 error, return the error message
+    			if(pieces.get(i-1).equals("error")) return "error: invalid entry";
+    			i-=2;
+    		}
+    	}
+    	//do all the addition and subtraction pieces
+    	for(int i=1; i<pieces.size();i+=2)
+    	{
+    		if(pieces.get(i).matches("[\\-+]"))
+    		{
+    			//Replace the three parts of the operation with the result
+    			pieces.set(i-1,performOperation(pieces.get(i-1),pieces.get(i),pieces.get(i+1)));
+    			pieces.remove(i);
+    			pieces.remove(i);
+    			pieces.trimToSize();
+    			//If the result was a divide by 0 error, return the error message
+    			if(pieces.get(i-1).equals("error")) return "error: invalid entry";
+    			i-=2;
+    		}
+    	}
     	
+    	return pieces.get(0);
+    }
+    
+    public static String performOperation(String num1,String oper,String num2)
+    {
     	//		Parsing first operand
     	
     	//Split the number into its separate parts
-    	String[] arg1string = pieces[0].split("[\\/_]");
+    	String[] arg1string = num1.split("[\\/_]");
     	//Convert to integers
     	int[] arg1 = new int[arg1string.length];
     	for(int i=0;i<arg1string.length;i++)
@@ -61,7 +111,7 @@ public class FracCalc {
     	//		Parsing second operand
 
     	//Split the number into its separate parts
-    	String[] arg2string = pieces[2].split("[\\/_]");
+    	String[] arg2string = num2.split("[\\/_]");
     	//Convert to integers
     	int[] arg2 = new int[arg2string.length];
     	for(int i=0;i<arg2string.length;i++)
@@ -99,7 +149,7 @@ public class FracCalc {
     	int numerator3 = 0;
     	int denominator3 = 1;
     	
-    	switch(pieces[1].charAt(0))
+    	switch(oper.charAt(0))
     	{
     	//Multiplication
     	case '*':
@@ -108,7 +158,7 @@ public class FracCalc {
     		break;
     	//Division
     	case '/':
-    		if(numerator2==0) return "error: divide by 0";
+    		if(numerator2==0) return "error";
     		numerator3 = numerator1*denominator2;
     		denominator3 = denominator1*numerator2;
     		break;
@@ -129,7 +179,7 @@ public class FracCalc {
     	int factor = gcd(numerator3,denominator3);
     	numerator3 /= factor;
     	denominator3 /= factor;
-    	if(denominator3<0) 
+    	if(denominator3<0)
 		{
 			numerator3 *=-1;
 			denominator3 *=-1;
